2dac898b255303ec8812f027c5368f95c5148f9a
Jacqui Kane <tjrkane@gmail.com>
2022-11-06 08:59:10 -0500

    initial commit

---
+++ jobs.js
@@ -0,0 +1,328 @@
+/** @format */
+jobs = [
+  {
+    category: "software",
+    role: "Software Developer",
+    skills: ["coding", "analysis", "problem solving", "communication"],
+    do: "develop solutions using software",
+    req: ["College Degree, Industry Credential"],
+    image: "images/softwaredeveloper.jpg",
+    desc: "Software developers may work with multiple computer programming languages, and different platforms. Front-end software developers write code for the user interface, back-end developers create code for servers and data management storage and retrieval. Full-stack developers work across the spectrum, doing front and back-end work.",
+  },
+  {
+    category: "software",
+    role: "IT Instructor",
+    skills: ["coding", "analysis", "problem solving", "communication"],
+    do: "teach next generation of computing professionals",
+    req: ["College Degree", "Masters in Computing"],
+    image: "images/instructor.png",
+    desc: "IT Instructors may teach a combination of software, hardware and computing concepts. College instructors require a masters degree or higher, in the content area, computing. High school or lower grade levels require a teaching certification consistent with district and state rules.",
+  },
+  {
+    category: "software",
+    role: "DevOps Engineer",
+    skills: ["coding", "testing", "deployment", "monitoring"],
+    do: "lead/coordinate activities to create and maintain company's software",
+    req: ["College Degree", "Industry Credential"],
+    image: "images/devops.png",
+    desc: "DevOps engineers are IT generalists, with wide knowledge of both development and operations, including coding, infrastructure management, system administration. DevOps engineers should also possess good interpersonal skills, to build a collaborative environment.",
+  },
+  {
+    category: "software",
+    role: "Web Developer",
+    skills: ["coding", "analysis", "problem solving", "communication"],
+    do: "create web sites using HTML and CSS, use platforms, like Vue.js, Angular",
+    req: ["College Degree", "Industry Credential"],
+    image: "images/webdeveloper.png",
+    desc: "Web developers build and maintain websites, using HTML, CSS and a variety of user interface and web libraries of code, like jQuery, Angular, Bootstrap.   Web developers often work alongside web designers to ensure that the design is well made and easy for users to navigate.",
+  },
+  {
+    category: "software",
+    role: "Full Stack Developer",
+    skills: [
+      "multiple platforms",
+      "coding",
+      "analysis",
+      "problem solving",
+      "communication",
+      "design",
+    ],
+    do: "front-end web and mobile apps, write APIs, server code, connect  with databases",
+    req: ["College Degree", "Industry Credential"],
+    image: "images/fullstackdeveloper.png",
+    desc: "Full Stack Developers work on both client-side and server-side software. They develop the Full Stack of a software application meaning Front end development, Back end development, Database, Server, API, and version controlling systems. Hence, the name Full Stack<em>Full Stack</em> Developer.",
+  },
+  {
+    category: "data",
+    role: "DB Administrator",
+    skills: [
+      "data",
+      "coding",
+      "analysis",
+      "problem solving",
+      "communication",
+      "design",
+    ],
+    do: "manage data, design/develop/maintain databases, troubleshoot, security",
+    req: ["College Degree", "Industry Credential"],
+    image: "images/databaseadmin.png",
+    desc: "Database administrators (DBA) are specialized computer systems administrators, responsible for maintaining a database and keeping data secure. A DBA's priority is maintaining data integrity, keeping it available to users and preventing unauthorized access.",
+  },
+  {
+    category: "cyber",
+    role: "Security Analyst",
+    skills: ["coding", "analysis", "problem solving", "communication"],
+    do: "secure company data and systems, filter suspicious activity, identify breach risks",
+    req: ["College Degree", "Industry Credential"],
+    image: "images/cybersecurity.png",
+    desc: "Security analysts detect and prevent cyber threats. Their job is to safeguard information, data, clients from cyber-attacks. They uncover weaknesses in the infrastructure and prevent sensitive data from being damaged, compromised or stolen.",
+  },
+  {
+    category: "network",
+    role: "Network Engineer",
+    skills: ["coding", "analysis", "problem solving", "communication"],
+    do: "design, build, manage computer networks within a company",
+    req: ["College Degree", "Industry Credential"],
+    image: "images/networkengineer.png",
+    desc: "Network engineers maintain and control computer networks and related computing environments. They troubleshoot, diagnose and resolve software, hardware, and other network and system problems, and may modify network structure or access to improve network performance.",
+  },
+  {
+    category: "data",
+    role: "Data Analyst",
+    skills: ["SQL", "analysis", "problem solving", "communication"],
+    do: "develop data collection systems, analyze data, talk with stakeholders",
+    req: ["College Degree - Math, Science", "Industry Credential"],
+    image: "images/dataanalyst.png",
+    desc: "Data analysts inspect, clean, transform, and model data to discover useful information, informing conclusions, and supporting decision-making. Data analysis encompasses many techniques, and is used in business, science, and social science domains.",
+  },
+  {
+    category: "data",
+    role: "Data Scientist",
+    skills: [
+      "SQL",
+      "math - statistics, probability",
+      "problem solving",
+      "communication",
+    ],
+    do: "use technology solutions, develop statistical and predictive models using data",
+    req: ["College Degree", "Industry Credential"],
+    image: "images/datascientist.png",
+    desc: "Data scientists work with business stakeholders to understand how data can be used to achieve goals. They design data modeling processes, create algorithms and predictive models to extract the data the business needs, and help analyze the data and share insights.",
+  },
+  {
+    category: "data",
+    role: "Data Modeler",
+    skills: ["SQL", "problem solving", "communication"],
+    do: "design and structure databases to optimize performance",
+    req: ["College Degree", "Industry Credential"],
+    image: "images/datamodeler.png",
+    desc: "Data modelers are analysts who work with data architects and database administrators to design computer databases. They play an integral role in helping organizations achieve end goals in nearly every area of operation, including product lifecycle and customer experience.",
+  },
+  {
+    category: "cyber",
+    role: "Penetration Tester",
+    skills: ["SQL", "problem solving", "communication"],
+    do: "identify security weaknesses, breach computer networks with company permission",
+    req: ["College Degree", "Industry Credential"],
+    image: "images/penetrationtester.png",
+    desc: "Penetration testers, pen testers or white-hat hackers, simulate cyberattacks against a computer system to find exploitable security vulnerabilities. Penetration testing helps organizations manage risk, protect clients from data breaches, and increase business continuity.",
+  },
+  {
+    category: "cyber",
+    role: "Cryptography Engineer",
+    skills: ["math", "algorithms", "problem solving", "communication"],
+    do: "secure data for information exchange, create encryption algorithms",
+    req: ["College Degree", "Industry Credential"],
+    image: "images/cryptographyengineer.png",
+    desc: "Cryptography engineers work with Cryptography and Identity systems. The work centers on troubleshooting complex system operations, on local and enterprise levels. It involves multiple operating systems, networking and cloud services as well as securely coordinating data access and retrieval.",
+  },
+  {
+    category: "cyber",
+    role: "Security Architect",
+    skills: [
+      "math",
+      "algorithms",
+      "problem solving",
+      "communication",
+      "programming",
+      "data management",
+    ],
+    do: "set protocols for security systems, threat research and policy development",
+    req: ["College Degree", "Industry Credential"],
+    image: "images/securityarchitect.png",
+    desc: "Security architects design security structures against malware and hacker intrusions to an organization's computer system. Once these structures are in place, the security architect coordinates testing for any weaknesses and regularly auditing the entire system.",
+  },
+  {
+    category: "network",
+    role: "Network Technician",
+    skills: ["math", "problem solving", "communication", "computer hardware"],
+    do: "implement network designs, build and maintain physical and digital connections ",
+    req: ["College Degree", "Industry Credential"],
+    image: "images/networktechnician.png",
+    desc: "Network Technicians manage installations and repairs for network systems. They respond to maintenance requests, monitor system defects and other issues, troubleshoot problems with network systems and maintain inventory for network hardware.",
+  },
+  {
+    category: "network",
+    role: "Network Architect",
+    skills: ["math", "problem solving", "communication", "computer hardware"],
+    do: "design network systems, manage networks, resolve network issues",
+    req: ["College Degree", "Industry Credential"],
+    image: "images/networkarchitect.png",
+    desc: "Network architects design and build data communication networks. These include local area networks, wide area networks, and intranets, which can range from a small connection to a multinational series of globally distributed communications systems.",
+  },
+  {
+    category: "network",
+    role: "Network Administrator",
+    skills: ["math", "problem solving", "communication", "computer hardware"],
+    do: "maintain networks, internet systems, local area networks and wide area networks",
+    req: ["College Degree", "Industry Credential"],
+    image: "images/networkadministrator.png",
+    desc: "Network administrators maintain and upgrade existing computer networks, including hardware like VPNs and routers, and access protocols for users on the network. They troubleshoot flaws in software, hardware configuration, communications equipment, and fix network issues.",
+  },
+  {
+    category: "software",
+    role: "UX/UI Developer",
+    skills: ["design", "problem solving", "communication", "programming"],
+    do: "design user interfaces(UI) to improve user experience(UX)",
+    req: ["College Degree", "Industry Credential"],
+    image: "images/uxuideveloper.png",
+    desc: "UI Developers work on designing and implementing websites, and ensuring that the appearance and functionality are optimized. UX (User Experience) Developers deal with the person's experience of using a website, and ensuring that the user has apositive experience visiting the site.",
+  },
+  {
+    category: "software",
+    role: "Systems Analyst",
+    skills: ["design", "problem solving", "communication", "programming"],
+    do: "identify business needs, develop software, program or security innovations",
+    req: ["College Degree", "Industry Credential"],
+    image: "images/systemsanalyst.png",
+    desc: "Systems analysts, are information technology professionals who specializes in analyzing, designing and implementing information systems. Systems analysts assess the suitability of information systems in terms of their intended outcomes and liaise with end users, stakeholders, software vendors and programmers.",
+  },
+  {
+    category: "software",
+    role: "Technical Writer",
+    skills: ["design", "problem solving", "communication", "programming"],
+    do: "describe how to use systems, and how they solve problems",
+    req: ["College Degree", "Industry Credential"],
+    image: "images/technicalwriter.png",
+    desc: "Technical writers use their writing and communication skills to make complex information easier to digest. They are essential to running a technologically focused business and can help companies better understand their performance and enhance the user experience.",
+  },
+];
+
+const createJobBox = (index) => {
+  let wrapper = $(".wrapper");
+  console.log(jobs[index].role);
+  let box = $(`<div class="box ${jobs[index].category}"></div>`);
+  let para = $(`<p class="care"></p>`);
+  let visual = $(`<img src="${jobs[index].image}">`);
+  visual.css({ width: "150px", height: "100px" });
+  box.append(visual);
+  box.append(para);
+  wrapper.append(box);
+
+  para.text(jobs[index].role);
+};
+
+//This will sort your array
+function SortByAttrib(a, b) {
+  let aAttrib = a.role.toLowerCase();
+  let bAttrib = b.role.toLowerCase();
+  return aAttrib < bAttrib ? -1 : aAttrib > bAttrib ? 1 : 0;
+}
+
+const showInformation = () => {
+  // Display the info for each job
+  // using the JSON data, with function establishSkillSet
+  jobs = jobs.sort(SortByAttrib);
+  $.each(jobs, createJobBox);
+
+  // Add an event handler for when the element of
+  // class box gets a click event,anonymous function here
+  $(".box").on("click", function () {
+    // Find the box clicked using the index method
+    let indexOfJob = $(".box").index(this);
+    console.log("index of jobs box ", indexOfJob);
+    // Find the paragraph of class p and set the text
+    $(this)
+      .find("p.care")
+      .text("Skills: " + jobs[indexOfJob].skills.join(", "));
+    // window.open(
+    //   "http://www.google.com",
+    //   "GoogleWindow",
+    //   "width=800, height=600"
+    // );
+    // Test:
+    let desc = jobs[indexOfJob].desc;
+    let title = jobs[indexOfJob].role;
+    let modbox = $("#myModal");
+    let modboxcontent = modbox.find(".modal-content p");
+    console.log("MODAL", modbox, modboxcontent);
+    let qualificationNeeds = jobs[indexOfJob].req.join(", ");
+    modboxcontent.html(
+      `<h2>${title}</h2><p>&#10169;${desc}</p><p class="highlight">Need: ${qualificationNeeds}</p>`
+    );
+    modbox.fadeIn(1000);
+    modbox.find(".close").click(function () {
+      modbox.fadeOut(1000);
+    });
+  });
+
+  // Add an event handler for when the element
+  // of class box gets a mouseover event
+  $(".box").on("mouseover", function () {
+    let indexOfJob = $(".box").index(this);
+    // decrease font size
+    $(this).find("p.care").text(jobs[indexOfJob].do).css("font-size", "small");
+  });
+
+  // Add an event handler for when the
+  // element of class box gets a mouseout event
+  $(".box").on("mouseout", function () {
+    let indexOfJob = $(".box").index(this);
+
+    $(this)
+      .find("p.care")
+      .text(jobs[indexOfJob].role)
+      .css("font-size", "large");
+  });
+
+  // turn on all checkboxes
+  let chks = $('input[type="checkbox"]');
+  chks.each(function () {
+    $(this).prop("checked", true);
+  });
+  console.log(chks);
+
+  // bind click event to checkboxes
+  $('input[type="checkbox"]').click(function () {
+    console.log("looking for : ", `.box.${$(this).val()}`);
+    let filtered = $(`.box.${$(this).val()}`);
+    console.log("##### filtered #####!!!! ", filtered);
+    if ($(this).prop("checked") == true) {
+      filtered.show();
+    } else if ($(this).prop("checked") == false) {
+      filtered.hide();
+    }
+  });
+
+  // Open Popup
+  $(".box").on("click", function () {
+    $(".popup").fadeIn(300);
+  });
+
+  // Close Popup
+  $(".box").on("click", function () {
+    $(".popup").fadeOut(300);
+  });
+
+  // Close Popup when Click outside
+  $(".popup")
+    .on("click", function () {
+      $(".popup").fadeOut(300);
+    })
+    .children()
+    .click(function () {
+      return false;
+    });
+};
+
+$(document).ready(showInformation);
